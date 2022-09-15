ea51af0679e88ad9cb0e95f3c3ef5ac9a497018a
jeck99 <jeck99@gmail.com>
2022-09-15 14:50:04 +0300

    first

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import "./Home.css";
+import IUser from "../../../models/user-model";
+
+const asd = (param: string): number => {
+  return 345;
+};
+function Home() {
+  let myUser: IUser;
+
+  return (
+    <div className="home">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
